ff58b19637d2941e5fe77b86dfb240e152504716
ishizu <demekinn_96@yahoo.co.jp>
2019-06-15 16:49:15 +0900

    fix: typo修正（blog --> blogs)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  #root to: 'blogs#index'
+
+  # ErrorCause: Routing Error
+  # FixReason: Fix Typo (blog --> blogs) 命名規約によりコントローラ名は複数形にする。
+
   resources :blog do
     resources :comments, only: [:create, :destroy]
   end
