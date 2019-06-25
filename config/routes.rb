2d48ef4c358afcfe4297b36267aab9fd625c3d01
ishizu <demekinn_96@yahoo.co.jp>
2019-06-25 11:33:49 +0900

    fix: ルーティング修正（blog --> blogs）

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  #root to: 'blogs#index'
-
+  root to: 'blogs#index'
   # ErrorCause: Routing Error
   # FixReason: Fix Typo (blog --> blogs) 命名規約によりコントローラ名は複数形にする。
 
-  resources :blog do
+  resources :blogs do
     resources :comments, only: [:create, :destroy]
   end
 end
